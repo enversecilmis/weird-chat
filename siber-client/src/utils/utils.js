0231d82e8b1fa5de82227430db1f1f40a7861d99
Enver <352563@ogr.ktu.edu.tr>
2022-03-28 23:49:35 +0300

    unexpected changes of pixels, not handled

--- siber-client/src/utils/utils.js
+++ siber-client/src/utils/utils.js
@@ -19,5 +19,49 @@ const setRGBA = (context, imgData, x, y, rgba) => {
     context.putImageData(imgData, 0, 0)
 }
 
+const getImage = (event, onLoad = (img64) => {}, width=150, height=150) => {
 
-export { getRGBA, setRGBA }
+    const data = event.dataTransfer?.getData("text/plain")
+    if(data){
+        const img = document.createElement('img')
+        img.onload = () => {
+            onLoad( resizeImage(img, width, height) )
+        }
+        img.src = data
+        return
+    }
+
+
+    const reader = new FileReader()
+    reader.onload = () => {
+        const img = document.createElement('img')
+        img.onload = () => onLoad( resizeImage(img, width, height) )
+        img.src = reader.result
+        // console.log(reader.result);
+    }
+    
+    event.dataTransfer?
+    reader.readAsDataURL(event.dataTransfer.files[0]):  
+    reader.readAsDataURL(event.target.files[0])
+    
+    // event.dataTransfer?
+    // reader.readAsArrayBuffer(event.dataTransfer.files[0]):  
+    // reader.readAsArrayBuffer(event.target.files[0])
+}
+
+
+
+const resizeImage = (img, width, height) => {
+    const canvas = document.createElement('canvas')
+    const ctx = canvas.getContext('2d')
+
+    canvas.width = width
+    canvas.height = height
+    ctx.drawImage(img, 0, 0, width, height)
+
+    
+    return canvas.toDataURL('image/jpg')
+}
+
+
+export { getRGBA, setRGBA, getImage }
